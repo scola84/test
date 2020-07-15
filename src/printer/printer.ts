5f005a189462be4052c36674a0b5217f13d8b887
M.C. van den Boogaart <scola@kvdb.name>
2020-07-15 18:08:52 +0200

    fix: remove value getter and setter

--- src/printer/printer.ts
+++ src/printer/printer.ts
@@ -5,16 +5,7 @@ export class Printer {
     this._value = value
   }
 
-  public getValue (): string {
-    return this._value
-  }
-
   public print (): string {
     return this._value
-  }
-
-  public setValue (value: string): this {
-    this._value = value
-    return this
   }
 }
